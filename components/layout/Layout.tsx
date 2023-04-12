b883c8fe067d0ebc97287a55caa9294bf50b8675
Kuba Sobecki <gosustyle@gmail.com>
2023-04-12 17:55:24 +0200

    Debug tooltips...

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import NoSsr from "./../helpers/NoSsr";
 
 interface Props {
   showHeader?: boolean;
@@ -19,7 +20,11 @@ export default function Layout({
       <main className="p-8 pt-12">
         <div className="mx-auto max-w-7xl">{children}</div>
       </main>
-      {showFooter && <Footer />}
+      {showFooter && (
+        <NoSsr>
+          <Footer />
+        </NoSsr>
+      )}
     </>
   );
 }
--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -6,7 +6,6 @@ import {
   AiOutlineWhatsApp,
 } from "react-icons/ai";
 import { FaTelegramPlane } from "react-icons/fa";
-import NoSsr from "./../../helpers/NoSsr";
 import { Tooltip } from "react-tooltip";
 import "react-tooltip/dist/react-tooltip.css";
 
@@ -55,74 +54,72 @@ export default function Footer() {
     <footer className="flex w-full flex-col items-center justify-center gap-y-4 bg-myDark p-8 text-sm text-white dark:bg-black/50 md:flex-row-reverse md:justify-between">
       <div className="flex space-x-2">
         <ErrorBoundary>
-          <NoSsr>
-            <a
-              href="https://github.com/kubasobecki/"
-              data-tooltip-id="tooltip-footer-github"
-              data-tooltip-content="GitHub"
-              target="_blank"
-              rel="noreferrer"
-              className="p-1 text-white/90 duration-300 hover:text-myLime"
-            >
-              <AiOutlineGithub size="24" />
-            </a>
-            <Tooltip id="tooltip-footer-github" style={tooltipStyle} />
-            <a
-              href="https://www.linkedin.com/in/kubasobecki/"
-              data-tooltip-id="tooltip-footer-linkedin"
-              data-tooltip-content="LinkedIn"
-              target="_blank"
-              rel="noreferrer"
-              className="p-1 text-white/90 duration-300 hover:text-myLime"
-            >
-              <AiFillLinkedin size="24" />
-            </a>
-            <Tooltip id="tooltip-footer-linkedin" style={tooltipStyle} />
-            <a
-              href="https://t.me/KubaSobecki"
-              data-tooltip-id="tooltip-footer-telegram"
-              data-tooltip-content="Telegram"
-              target="_blank"
-              rel="noreferrer"
-              className="p-1 text-white/90 duration-300 hover:text-myLime"
-            >
-              <FaTelegramPlane size="24" />
-            </a>
-            <Tooltip id="tooltip-footer-telegram" style={tooltipStyle} />
-            <a
-              href="https://wa.me/48519676788"
-              data-tooltip-id="tooltip-footer-whatsapp"
-              data-tooltip-content="WhatsApp"
-              target="_blank"
-              rel="noreferrer"
-              className="p-1 text-white/90 duration-300 hover:text-myLime"
-            >
-              <AiOutlineWhatsApp size="24" />
-            </a>
-            <Tooltip id="tooltip-footer-whatsapp" style={tooltipStyle} />
-            <a
-              href="mailto:kubasobecki@gmail.com"
-              data-tooltip-id="tooltip-footer-email"
-              data-tooltip-content="Email"
-              target="_blank"
-              rel="noreferrer"
-              className="p-1 text-white/90 duration-300 hover:text-myLime"
-            >
-              <AiFillMail size="24" />
-            </a>
-            <Tooltip id="tooltip-footer-email" style={tooltipStyle} />
-            <a
-              href="tel:+48519676788"
-              data-tooltip-id="tooltip-footer-mobile"
-              data-tooltip-content="Mobile"
-              target="_blank"
-              rel="noreferrer"
-              className="p-1 text-white/90 duration-300 hover:text-myLime"
-            >
-              <AiFillPhone size="24" />
-            </a>
-            <Tooltip id="tooltip-footer-mobile" style={tooltipStyle} />
-          </NoSsr>
+          <a
+            href="https://github.com/kubasobecki/"
+            data-tooltip-id="tooltip-footer-github"
+            data-tooltip-content="GitHub"
+            target="_blank"
+            rel="noreferrer"
+            className="p-1 text-white/90 duration-300 hover:text-myLime"
+          >
+            <AiOutlineGithub size="24" />
+          </a>
+          <Tooltip id="tooltip-footer-github" style={tooltipStyle} />
+          <a
+            href="https://www.linkedin.com/in/kubasobecki/"
+            data-tooltip-id="tooltip-footer-linkedin"
+            data-tooltip-content="LinkedIn"
+            target="_blank"
+            rel="noreferrer"
+            className="p-1 text-white/90 duration-300 hover:text-myLime"
+          >
+            <AiFillLinkedin size="24" />
+          </a>
+          <Tooltip id="tooltip-footer-linkedin" style={tooltipStyle} />
+          <a
+            href="https://t.me/KubaSobecki"
+            data-tooltip-id="tooltip-footer-telegram"
+            data-tooltip-content="Telegram"
+            target="_blank"
+            rel="noreferrer"
+            className="p-1 text-white/90 duration-300 hover:text-myLime"
+          >
+            <FaTelegramPlane size="24" />
+          </a>
+          <Tooltip id="tooltip-footer-telegram" style={tooltipStyle} />
+          <a
+            href="https://wa.me/48519676788"
+            data-tooltip-id="tooltip-footer-whatsapp"
+            data-tooltip-content="WhatsApp"
+            target="_blank"
+            rel="noreferrer"
+            className="p-1 text-white/90 duration-300 hover:text-myLime"
+          >
+            <AiOutlineWhatsApp size="24" />
+          </a>
+          <Tooltip id="tooltip-footer-whatsapp" style={tooltipStyle} />
+          <a
+            href="mailto:kubasobecki@gmail.com"
+            data-tooltip-id="tooltip-footer-email"
+            data-tooltip-content="Email"
+            target="_blank"
+            rel="noreferrer"
+            className="p-1 text-white/90 duration-300 hover:text-myLime"
+          >
+            <AiFillMail size="24" />
+          </a>
+          <Tooltip id="tooltip-footer-email" style={tooltipStyle} />
+          <a
+            href="tel:+48519676788"
+            data-tooltip-id="tooltip-footer-mobile"
+            data-tooltip-content="Mobile"
+            target="_blank"
+            rel="noreferrer"
+            className="p-1 text-white/90 duration-300 hover:text-myLime"
+          >
+            <AiFillPhone size="24" />
+          </a>
+          <Tooltip id="tooltip-footer-mobile" style={tooltipStyle} />
         </ErrorBoundary>
       </div>
       <span className="text-white/90">
