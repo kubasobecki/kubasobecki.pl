c6285a60f30aa7aaba4e8cbbde2e2920382b441f
Kuba Sobecki <gosustyle@gmail.com>
2023-04-12 16:45:28 +0200

    Test ErrorBoundary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,39 @@ import Head from "next/head";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
 
+import React, { Component, ErrorInfo, ReactNode } from "react";
+
+interface Props {
+  children?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+  };
+
+  public static getDerivedStateFromError(_: Error): State {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return <h1>Sorry.. there was an error</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
@@ -36,7 +69,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
-      <Component {...pageProps} />
+      <ErrorBoundary>
+        <Component {...pageProps} />
+      </ErrorBoundary>
     </Provider>
   );
 }
