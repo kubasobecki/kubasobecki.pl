7ec353173feb501ed2c52ee979a7b8ed1c4d19a2
Kuba Sobecki <gosustyle@gmail.com>
2023-04-12 17:30:17 +0200

    Debug footer icons

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -64,7 +64,7 @@ export default function Footer() {
           >
             {/* <AiOutlineGithub size="24" /> */}
           </a>
-          <Tooltip id="tooltip-footer-github" style={tooltipStyle} />
+          {/* <Tooltip id="tooltip-footer-github" style={tooltipStyle} /> */}
           <a
             href="https://www.linkedin.com/in/kubasobecki/"
             data-tooltip-id="tooltip-footer-linkedin"
@@ -75,7 +75,7 @@ export default function Footer() {
           >
             {/* <AiFillLinkedin size="24" /> */}
           </a>
-          <Tooltip id="tooltip-footer-linkedin" style={tooltipStyle} />
+          {/* <Tooltip id="tooltip-footer-linkedin" style={tooltipStyle} /> */}
           <a
             href="https://t.me/KubaSobecki"
             data-tooltip-id="tooltip-footer-telegram"
@@ -86,7 +86,7 @@ export default function Footer() {
           >
             {/* <FaTelegramPlane size="24" /> */}
           </a>
-          <Tooltip id="tooltip-footer-telegram" style={tooltipStyle} />
+          {/* <Tooltip id="tooltip-footer-telegram" style={tooltipStyle} /> */}
           <a
             href="https://wa.me/48519676788"
             data-tooltip-id="tooltip-footer-whatsapp"
@@ -97,7 +97,7 @@ export default function Footer() {
           >
             {/* <AiOutlineWhatsApp size="24" /> */}
           </a>
-          <Tooltip id="tooltip-footer-whatsapp" style={tooltipStyle} />
+          {/* <Tooltip id="tooltip-footer-whatsapp" style={tooltipStyle} /> */}
           <a
             href="mailto:kubasobecki@gmail.com"
             data-tooltip-id="tooltip-footer-email"
@@ -108,7 +108,7 @@ export default function Footer() {
           >
             {/* <AiFillMail size="24" /> */}
           </a>
-          <Tooltip id="tooltip-footer-email" style={tooltipStyle} />
+          {/* <Tooltip id="tooltip-footer-email" style={tooltipStyle} /> */}
           <a
             href="tel:+48519676788"
             data-tooltip-id="tooltip-footer-mobile"
@@ -119,7 +119,7 @@ export default function Footer() {
           >
             {/* <AiFillPhone size="24" /> */}
           </a>
-          <Tooltip id="tooltip-footer-mobile" style={tooltipStyle} />
+          {/* <Tooltip id="tooltip-footer-mobile" style={tooltipStyle} /> */}
         </ErrorBoundary>
       </div>
       <span className="text-white/90">
